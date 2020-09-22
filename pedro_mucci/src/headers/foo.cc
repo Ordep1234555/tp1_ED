e7ef3dcd396eb05158acbf4073dba9d044b9af12
Ordep1234555 <48034949+Ordep1234555@users.noreply.github.com>
2020-09-22 10:49:46 -0300

    Update foo.cc

--- pedro_mucci/src/headers/foo.cc
+++ pedro_mucci/src/headers/foo.cc
@@ -2,3 +2,32 @@
 int bar() {
   return 1;
 }
+#include "../header/pilha.hpp"
+#include <iostream>
+
+using namespace std;
+
+void Pilha::empilha(int nave) {
+    if(_tamanho == MAXTAM) {
+        throw "A pilha está cheia!";
+    }
+    _topo++;
+    _arrayFrota[_topo] = nave;
+    _tamanho++;
+}
+
+int Pilha::desempilha() {
+    int aux;
+    if(_tamanho == 0) {
+        throw "A pilha está vazia!";
+    }
+    aux = _arrayFrota[_topo];
+    _topo--;
+    _tamanho--;
+    return aux;
+}
+
+void Pilha::limpa() {
+    _tamanho = 0;
+    _topo = -1;
+}
