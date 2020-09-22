e264950fb060ada10b4223952057263fe016d51b
Ordep1234555 <48034949+Ordep1234555@users.noreply.github.com>
2020-09-22 10:52:49 -0300

    Update foo.h

--- pedro_mucci/src/headers/foo.h
+++ pedro_mucci/src/headers/foo.h
@@ -6,3 +6,24 @@
 int bar(); 
 
 #endif
+#ifndef PILHA_H
+#define PILHA_H
+
+#include <iostream>
+
+class Pilha {
+    public:
+        Pilha(){_tamanho = 0; _topo = -1;};
+        int getTamanho() {return _tamanho;};
+        bool vazia() {return _tamanho == 0;};
+        void empilha(int nave);
+        int desempilha();
+        void limpa();
+    private:
+        int _tamanho;
+        int _topo;
+        static const int MAXTAM = 5000;
+        int _arrayFrota[MAXTAM];
+};
+
+#endif
