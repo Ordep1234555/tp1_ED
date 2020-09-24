47ec4efa3236dd4d7e113c81c2f5a9103396be95
Ordep1234555 <48034949+Ordep1234555@users.noreply.github.com>
2020-09-24 11:42:14 -0300

    Update main.cc

--- pedro_mucci/src/main.cc
+++ pedro_mucci/src/main.cc
@@ -1,18 +1,43 @@
 #include <cstdio> // em C substituir por #include <stdio.h>
-#include "headers/foo.h" //um exemplo de como incluir cabeçalhos de outros arquivos criados
+#include <iostream>
+#include "string.h"
+#include "headers/TAD.hpp" //um exemplo de como incluir cabeçalhos de outros arquivos criados
+#define  MAXTAM = 5000;
+
+using namespace std;
 
 int main() {
+  
   int num_frotas;
-  scanf("%d",&num_frotas);
-  int i;
-  for(i=0; i < num_frotas; i++){
+  cin >> num_frotas;
+  int aux;
+  for(aux=0; aux < num_frotas; aux++){
     int id_nave;
-    scanf("%d",&id_nave);
-    //desenvolver o código para inserção correta das naves que são lidas
+    cin >> id_nave;
+    Pilha.Empilha(id_nave);
   }
-  int operacao;
-  while(scanf("%d", &operacao) != EOF) {
-    //desenvolver o código relacionado as operações a serem realizadas
+  
+  int operacao,aux2;
+  while(cin>>operação != EOF) {
+    if(comando ==0){
+      aux2= Pilha.Desempilha();
+      Lista.InsereFinal(aux2);
+      cout << "nave " + to_string(item) + " em combate" << endl;
+    } else if (comando == -1) {
+      aux2= Fila.Desenfileira();
+      Pilha.Empilha(aux2);
+      cout << "nave " + to_string(item) + " consertada" << endl;      
+    } else if (comando == -2) {
+      Pilha.Imprime;
+    } else if (comando == -3) {
+      Fila.Imprime;
+    } else {
+      pos = Lista.Pesquisa(comando);
+      aux2 = Lista.RemovePosicao(pos + 1);
+      Fila.Enfileira(aux2);
+      cout << "nave " + to_string(item) + " avariada" << endl;
+    }
+      
   }
   return 0;
 }
