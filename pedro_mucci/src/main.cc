a731f18ad99160e56dceda2f3dc98cba229f5160
Ordep1234555 <48034949+Ordep1234555@users.noreply.github.com>
2020-09-24 20:35:48 -0300

    :)

--- pedro_mucci/src/main.cc
+++ pedro_mucci/src/main.cc
@@ -1,7 +1,7 @@
-#include <cstdio> // em C substituir por #include <stdio.h>
+#include <cstdio>
 #include <iostream>
 #include "string.h"
-#include "headers/TAD.hpp" //um exemplo de como incluir cabeçalhos de outros arquivos criados
+#include "headers/TAD.hpp"
 
 using namespace std;
 
@@ -14,33 +14,44 @@ int main() {
   int num_frotas;
   cin >> num_frotas;
   int aux;
-  for(aux=0; aux < num_frotas; aux++){
-    int id_nave;
-    cin >> id_nave;
-    Pilha.Empilha(id_nave);
-  }
-  
-  int operacao,aux2,pos;
-  while(cin>>operacao) {
-    if(operacao ==0){
-      aux2= Pilha.Desempilha();
-      Lista.InsereFinal(aux2);
-      cout << "nave " + to_string(aux2) + " em combate" << endl;
-    } else if (operacao == -1) {
-      aux2= Fila.Desenfileira();
-      Pilha.Empilha(aux2);
-      cout << "nave " + to_string(aux2) + " consertada" << endl;      
-    } else if (operacao == -2) {
-      Pilha.Imprime();
-    } else if (operacao == -3) {
-      Fila.Imprime();
-    } else {
-      pos = Lista.Pesquisa(operacao);
-      aux2 = Lista.RemovePosicao(pos + 1);
-      Fila.Enfileira(aux2);
-      cout << "nave " + to_string(aux2) + " avariada" << endl;
+  for (aux = 0; aux < num_frotas; aux++)
+    {
+      int id_nave;
+      cin >> id_nave;
+      Pilha.Empilha (id_nave);
     }
-      
-  }
+
+  int operacao, aux2, pos;
+  while (cin >> operacao)
+    {
+      if (operacao == 0)
+	{
+	  aux2 = Pilha.Desempilha ();
+	  Lista.InsereFinal (aux2);
+	  cout << "nave " + to_string (aux2) + " em combate" << endl;
+	}
+      else if (operacao == -1)
+	{
+	  aux2 = Fila.Desenfileira ();
+	  Pilha.Empilha (aux2);
+	  cout << "nave " + to_string (aux2) + " consertada" << endl;
+	}
+      else if (operacao == -2)
+	{
+	  Pilha.Imprime ();
+	}
+      else if (operacao == -3)
+	{
+	  Fila.Imprime ();
+	}
+      else
+	{
+	  pos = Lista.Pesquisa (operacao);
+	  aux2 = Lista.RemovePosicao (pos + 1);
+	  Fila.Enfileira (aux2);
+	  cout << "nave " + to_string (aux2) + " avariada" << endl;
+	}
+
+    }
   return 0;
 }
